bf23cb0360b3a640298bc4a55590de1353e6d30c
nainai <nainaixiong0725@gmail.com>
2026-05-02 16:10:46 +0800

    add basic emotion recovery
    Added emotional recovery method to Peep class.

--- peeple.py
+++ peeple.py
@@ -1,10 +1,3 @@
-#
-# Author:
-# ID:
-#
-# peeple.py - Class definitions for emotion simulation peeps
-#
-
 import random
 
 
@@ -28,10 +21,36 @@ class Peep:
         self.angry = 10
 
     def __str__(self):
-        return self.name + " @ " + str(self.pos)
+        return (
+            self.name + " @ " + str(self.pos)
+            + " happy=" + str(self.happy)
+            + " sad=" + str(self.sad)
+            + " angry=" + str(self.angry)
+        )
 
     def get_pos(self):
         return self.pos
+
+    def recover_emotions(self):
+        """Slowly return emotions to their base values."""
+
+        # Happy returns to base level 50.
+        if self.happy < 50:
+            self.happy += 1
+        if self.happy > 50:
+            self.happy -= 1
+
+        # Sad returns to base level 10.
+        if self.sad < 10:
+            self.sad += 1
+        if self.sad > 10:
+            self.sad -= 1
+
+        # Angry returns to base level 10.
+        if self.angry < 10:
+            self.angry += 1
+        if self.angry > 10:
+            self.angry -= 1
 
     def step_change(self, world):
         """Move the peep randomly, but do not move into barriers."""
@@ -57,3 +76,6 @@ class Peep:
         # The peep only moves if the new cell is not a barrier.
         if world[new_x, new_y] != 11:
             self.pos = (new_x, new_y)
+
+        # Update emotional recovery every timestep.
+        self.recover_emotions()
